d0a49bbd5482cf508c5239e7411aef38be98ee01
piesimon <dusedoris0728726780@gmail.com>
2022-05-23 06:53:51 -0700

    work on index page

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Suggestion System</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <style>
+  .title{
+    background: whitesmoke;
+    height:100px;
+     width:1035px;
+     margin-top: 0px;
+     margin-left: 0px;
+     color:green;
+margin-right:0px;
+  }
+.mama
+{
+height:300px;
+width:600px;
+background: whitesmoke;
+margin-top: 80px;
+margin-left: 250px;
+margin-right:100px;
+border-radius:20px;
+border: solid green;
+color:green;
+font-family: ;
+padding: 100px 30px;
+}
+  
+  </style>
+  </head>
+  <body class="bg-info">
+  <div class="title"><br>
+  <center><h2>Students Suggestion Portal</h2></center>
+  
+  </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="admin_login.php">Admin Login <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="student_login_page.php">Student Login</a>
+      </li>
+      
+      
+    </ul>
+  </div>
+</nav>
+<div class="mama">
+<p>   This system help students to send their ideas to school student <br>
+                     affair office in easy way
+
+<br>
+
+
+ </p>
+
+</div>
+
+
+
+</div>
