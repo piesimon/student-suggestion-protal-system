bc7db13b8b5a9ffd970261796c7f957ed3e845e1
piesimon <dusedoris0728726780@gmail.com>
2022-05-23 07:47:34 -0700

    index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Student suggestion portal System</title>
+    <title>Students suggestion portal System</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
