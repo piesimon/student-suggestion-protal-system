cfa3b9d1b8554be9cdf0d76d0f31a12007747a41
piesimon <dusedoris0728726780@gmail.com>
2022-05-23 03:14:20 -0700

    first commit by simon

---
+++ admin_login.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>SUGGESTION SYSTEM</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+<br><br><br><br><br>
+<div class="container">
+    <div class="card nt-5">
+
+        <div class="card-header">
+
+            <h2> Admin Login</h2>
+</div>
+
+<div class="card-body">
+<form method="post" action="login_verify1.php">
+<div class="form-group">
+        <label for="name">Username</label>
+        <input type="text" name="username" id="id" class="form-control" placeholder="username">
+</div>
+
+<div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="id" class="form-control" placeholder="password">
+</div>
+<div class="form-group">
+    <button type="submit" class="btn btn-info" name="login"> Login</button>
+    <button type="reset" class="btn btn-info"> clear</button>
+</div>
+</form>
+</div>
+</div>
+</div>
